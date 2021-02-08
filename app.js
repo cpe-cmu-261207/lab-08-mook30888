373a678d6529d7f6c0ee732ab1a2c5139c6b2539
mook30888 <mookkak@gmail.com>
2021-02-08 22:48:26 +0700

    gg

--- app.js
+++ app.js
@@ -6,15 +6,44 @@ const fs = require("fs");
 //to post you must use bodyParser
 
 app.use(express.static("assets"));
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.get("/", (req, res) => {
   res.end(fs.readFileSync("./instruction.html"));
 });
 
 //implement your api here
 //follow instruction in http://localhost:8000/
-
 const saved = require("./myCourses");
+
+const calc = () => {
+  let gpax = saved.courses.map(course => {
+    return {
+      gpa: Number(course.gpa) * Number(course.credit),
+      credit: Number(course.credit)
+    }
+  })
+    .reduce((sum, course) => {
+      return {
+        gpa: course.gpa + sum.gpa,
+        credit: course.credit + sum.credit
+      }
+    }, { gpa: 0, credit: 0 })
+  console.log(gpax)
+  saved.gpax = (gpax.gpa / gpax.credit).toFixed(2)
+  console.log(saved.gpax)
+}
+
+const writeJSON = () => {
+  let db = JSON.stringify(saved, null, 2)
+  fs.writeFileSync('myCourses.json', db)
+}
+
+const sync = () => {
+  calc()
+  writeJSON()
+}
+
+
 
 
 app.get("/courses", (req, res) => {
@@ -46,17 +75,19 @@ app.delete("/courses/:id", (req, res) => {
   let len = saved.courses.length
   saved.courses = (saved.courses.filter(e => e.courseId != req.params.id));
   if(saved.courses.length < len ){
+    sync();
     res.json({
       success: true,
       data: saved.courses
     })
+    
   }else{
     res.json({
       success: false,
       data: saved.courses
     })
   }
-
+  
 });
 
 app.post("/addCourse", (req, res) => {
@@ -65,6 +96,7 @@ app.post("/addCourse", (req, res) => {
   if(courseId !== undefined && courseName !== undefined && credit !== undefined && gpa !== undefined){
     const course = {courseId: courseId , courseName: courseName, credit: credit, gpa:gpa }
     saved.courses.push(course)
+    sync();
     res.status(201).json({
       success: true,
       data: course
